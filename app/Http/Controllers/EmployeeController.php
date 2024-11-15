52268352bc8f0cd393403ab22502d73d4a14cc2e
PeshkovDmitry <peshkov-dv@yandex.ru>
2024-11-15 19:47:17 +0300

    Почти готово

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -7,5 +7,45 @@ use Illuminate\Http\Request;
 
 class EmployeeController extends Controller
 {
-    //
+    
+    public function index() {
+        return view('form');
+    }
+
+    public function store(Request $request) {
+
+        $name = $request->input('name');
+        $lastname = $request->input('lastname');
+        $position = $request->input('position');
+        $email = $request->input('email');
+        $workData = $request->input('workData');
+
+        $data = json_decode($workData);
+        $street = $data->address->street;
+        $lat = $data->address->geo->lat; 
+
+        echo $name . ' ' . $lastname . ' ' . $position . ' ' . $email;
+        echo "<br>";
+        echo $street;
+        echo "<br>";
+        echo $lat;
+        echo "<br>";
+        echo $this->getPath($request);
+        echo "<br>";
+        echo $this->getUrl($request);
+
+    }
+
+    public function update(Request $request, $id) {
+        echo $id;
+    }
+
+    private function getPath(request $request) {
+        return $request->path();
+    }
+
+    private function getUrl(Request $request) {
+        return $request->url();
+    }
+
 }
--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -5,16 +5,24 @@
         <form name="employee-form" id="emlpoyee-form" method="post" action="{{ url('store-form') }}">
             @csrf
             <div class="form-group">
-                <label for="name">Name</label>
+                <label for="name">Имя</label>
                 <input type="text" name="name" id="name" class="form-control" required="true">
             </div>
             <div class="form-group">
-                <label for="email">Description</label>
+                <label for="lastname">Фамилия</label>
+                <input type="text" name="lastname" id="lastname" class="form-control" required="true">
+            </div>
+            <div class="form-group">
+                <label for="position">Должность</label>
+                <input type="text" name="position" id="position" class="form-control" required="true">
+            </div>
+            <div class="form-group">
+                <label for="email">Почта</label>
                 <input type="email" name="email" id="email" class="form-control" required="true">
             </div>
             <div class="form-group">
-                <label for="workData">workData</label>
-                <textarea name="workData" class="form-control" required="true"></textarea>
+                <label for="workData">Информация</label>
+                <textarea name="workData" class="form-control" cols="30" rows="30" required="true"></textarea>
             </div>
             <button type="submit" class="btn btn-primary">Submit</button>
         </form>
